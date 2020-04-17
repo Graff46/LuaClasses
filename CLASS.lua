8718a5daba0e0ea1296f6433f3ba992623acf152
Graff46 <macter46@yandex.ru>
2020-04-17 20:19:59 +0300

    Add general file

---
+++ CLASS.lua
@@ -0,0 +1,51 @@
+local function noop(...) return end
+
+function createClass(name, static)
+  local model = {}
+  local interface = {}
+  
+  interface.Init = noop
+  interface.finalize = noop
+  
+  function _caller(obj, ...)
+    obj.Init(...)
+    --print "_caller"
+    return model
+  end
+  
+  function _finaliter(obj, ...)
+    return obj.finalize(...)
+    --print "_finaliter"
+  end
+  
+  if static then
+    setmetatable(model, {__gc = _finaliter})
+    _G[name] = model
+  else
+    setmetatable(interface,
+      {
+        __call = _caller,
+        __gc = _finaliter,
+        __newindex = model
+      })
+
+    _G[name] = interface
+  end
+  
+  return model
+end
+
+function class(className, static)
+  local signature = createClass(className, static)
+
+  return function(ParentClass) 
+    for key, value in pairs(ParentClass) do
+      signature[key] = value
+    end
+    return signature
+  end
+end
+
+function structura(className)
+  return class(className, true)
+end
